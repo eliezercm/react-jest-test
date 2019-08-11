6f029d224468d1bf857cdd74b7914043e50b8e3c
Eliezer Martins <eliezermarttins@gmail.com>
2019-08-11 17:32:07 -0300

    fixing test description

--- src/__tests__/store/reducers/techs.test.js
+++ src/__tests__/store/reducers/techs.test.js
@@ -2,7 +2,7 @@ import reducer, { INITIAL_STATE } from "~/store/modules/techs/reducer";
 import * as Techs from "~/store/modules/techs/actions";
 
 describe("Techs reducer", () => {
-  it("DEFAULT | should not fail when pass empty actions or undefined state", () => {
+  it("DEFAULT | state should be equals to INITIAL_STATE when pass empty actions", () => {
     const state = reducer(undefined, {});
 
     expect(state).toStrictEqual(INITIAL_STATE);
